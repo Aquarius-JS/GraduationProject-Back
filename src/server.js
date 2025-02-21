039036e64b2070e26a21760a6ad80a650f4bfb21
aquarius <1015772292@qq.com>
2025-02-21 20:56:05 +0800

    feat(跨域)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors())
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('access-control-allow-headers', 'content-type');
+  res.setHeader('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+  console.log(req)
+  res.json({ isOk: 'yes' })
 })
 
 app.listen(port, () => {
