0e26e92cec2678ea0a39a0744518cd06769c0206
aquarius <1015772292@qq.com>
2025-02-23 23:20:12 +0800

    feat(getUserInfo)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { login } = require('./controller/user');
+const { login, getUserInfo } = require('./controller/user');
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Origin', origin ?? '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('access-control-allow-headers', 'content-type');
   res.setHeader('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
@@ -16,6 +16,7 @@ app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
 
 app.post('/login', login);
+app.get('/getUserInfo', getUserInfo);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
--- src/controller/user.js
+++ src/controller/user.js
@@ -2,6 +2,11 @@ const jwt = require('jsonwebtoken');
 const privateKey = require('../config/privateKey');
 const { getUserInfoByStuNumber } = require('../model/userInfo');
 
+/**
+ * 根据学号密码进行登录
+ * @param {Object} req
+ * @param {Object} res
+ */
 async function login(req, res) {
   const { stuNumber, password } = req.body;
   const info = await getUserInfoByStuNumber(stuNumber);
@@ -15,4 +20,17 @@ async function login(req, res) {
   }
 }
 
+/**
+ * 根据登录token获取用户信息
+ * @param {Object} req
+ * @returns
+ */
+async function getUserInfo(req, res) {
+  const token = req.cookies.token;
+  const { stuNumber } = jwt.verify(token, privateKey);
+  const userInfo = await getUserInfoByStuNumber(stuNumber);
+  res.json(userInfo);
+}
+
 exports.login = login;
+exports.getUserInfo = getUserInfo;
