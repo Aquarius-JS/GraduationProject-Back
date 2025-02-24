60e8bf34afe61f5a316aed5fb267a830c3393cc0
aquarius <1015772292@qq.com>
2025-02-24 23:50:05 +0800

    refact(抽离cors逻辑)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const myCors = require('./middleware/cors');
 const { isLoginMiddleware } = require('./middleware/user');
 const { login, getUserInfo } = require('./controller/user');
 const { port } = require('./config/appConfig');
 const app = express();
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  res.setHeader('Access-Control-Allow-Origin', origin ?? '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('access-control-allow-headers', 'content-type');
-  res.setHeader('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  next();
-}); // for CORS
+app.use(myCors); // for CORS
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
 app.use(isLoginMiddleware); // for checking login status
---
+++ src/middleware/cors.js
@@ -0,0 +1,16 @@
+/**
+ * 跨域cors中间件
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+const myCors = (req, res, next) => {
+  const origin = req.headers.origin;
+  res.setHeader('Access-Control-Allow-Origin', origin ?? '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('access-control-allow-headers', 'content-type');
+  res.setHeader('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+};
+
+module.exports = myCors;
