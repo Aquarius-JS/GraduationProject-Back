5ee5d37ca28a37c4f0c6594a444e31b0b8ca4f79
aquarius <1015772292@qq.com>
2025-02-24 16:46:29 +0800

    feat(cookie 过期时间)

--- src/controller/user.js
+++ src/controller/user.js
@@ -13,7 +13,7 @@ async function login(req, res) {
   const stuInfo = info[0];
   if (stuInfo && stuInfo.password === password) {
     const token = jwt.sign({ stuNumber }, privateKey);
-    res.cookie('token', token, { httpOnly: false });
+    res.cookie('token', token, { httpOnly: false, maxAge: 1000 * 60 * 60 * 24 * 7 });
     res.json({ isOk: true });
   } else {
     res.json({ isOk: false });
