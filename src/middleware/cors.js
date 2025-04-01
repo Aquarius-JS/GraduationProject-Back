ab220d2874b4171ec752477e57fbf4951b87d2ca
aquarius <1015772292@qq.com>
2025-04-01 23:26:28 +0800

    fix(修复文件后缀名解析错误bug)

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -8,7 +8,7 @@ const myCors = (req, res, next) => {
   const origin = req.headers.origin;
   res.setHeader('Access-Control-Allow-Origin', origin ?? '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('access-control-allow-headers', 'content-type');
+  res.setHeader('access-control-allow-headers', 'content-type,suffix');
   res.setHeader('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 };
--- src/service/saveFile.js
+++ src/service/saveFile.js
@@ -3,7 +3,7 @@ const curUnixDate = require('./share/curUnixDate');
 
 async function saveFile(req) {
   const buffer = req.body;
-  const fileType = req.headers['content-type'].split('/')[1];
+  const fileType = req.headers.suffix ?? '';
   const fileName = `${curUnixDate()}.${fileType}`;
   const filePath = `${__dirname}/../upload/${fileName}`;
   return new Promise((resolve, reject) => {
