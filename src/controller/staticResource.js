f8b7216edd20dd5e59a7775816190b8cea99a212
aquarius <1015772292@qq.com>
2025-05-31 18:42:14 +0800

    refactor(url配置)

--- src/controller/staticResource.js
+++ src/controller/staticResource.js
@@ -1,12 +1,15 @@
 const saveFile = require('../service/saveFile');
+const { port } = require('../config/appConfig');
+
 /**
  * 静态资源上传接口
  * @param {*} req
  * @param {*} res
  */
 async function uploadFile(req, res) {
+  const host = process.env.HOST || 'localhost';
   const fileName = await saveFile(req);
-  res.json({ fileName, url: `http://localhost:3000/upload/${fileName}` });
+  res.json({ fileName, url: `http://${host}:${port}/upload/${fileName}` });
 }
 
 exports.uploadFile = uploadFile;
